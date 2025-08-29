95da5d8602032e814a0580e291df555215032c57
Detol <naufal3544@gmail.com>
2025-08-29 11:08:39 +0700

    update baru

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Router, Route, A} from "@solidjs/router";
 import SignIn from "./pages/Signin"
 import SignUp from "./pages/Signup"
-import Dashboard from "./pages/Dashboard";
-import OSP from "./pages/OSP";
 
 export default  function App() {
   return(
@@ -10,9 +8,6 @@ export default  function App() {
       <Router>
         <Route path="/signin" component={SignIn}/>
         <Route path="/signup" component={SignUp}/>
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/osp" component={OSP} />
-        <Route path="/" component={() => <Dashboard />} />
       </Router>
     </>
   )
